7d4d240dca040086b3aa42e670befef9872eaced
JonathanBorics <jonathanborics97@gmail.com>
2026-01-17 15:56:41 +0100

    Update APK v5 with expo-notifications plugin

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
 
         <motion.div variants={itemVariants} className="flex flex-col items-center gap-6">
           <motion.a
-            href="https://expo.dev/artifacts/eas/iDimTdnrdi7UfJ1mXPEe49.apk"
+            href="https://expo.dev/artifacts/eas/7XQKDKxxEKzfyUD3bH98CT.apk"
             target="_blank"
             whileHover={{ scale: 1.10 }}
             whileTap={{ scale: 0.95 }}
